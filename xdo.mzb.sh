11c6ec87f9cb7a753629ace340d8e4f17d481993
José Almeida <jose.afga@gmail.com>
2019-08-06 02:44:05 -0300

    add xdo

---
+++ xdo.mzb.sh
@@ -0,0 +1,33 @@
+# xdo mzbuild 0.1.0a
+#
+# contribuitors:
+#   José Almeida <jose.afga@gmail.com>
+
+# package info
+NAME='xdo'
+VERSION="${VERSION:-0.5.7}"
+BUILD="${BUILD:-1}"
+DESCRIPTION='Small X utility to perform elementary actions on windows.'
+LICENSE='BSD'
+HOMEPAGE='https://github.com/baskerville/xdo'
+# personal info
+MAINTAINER='José Almeida <jose.afga@gmail.com>'
+# download info
+URL="https://github.com/baskerville/xdo/archive/${VERSION}.tar.gz"
+CHECKSUM_MD5='26a864375c6e703b87a2ff6d6f7a6068'
+#CHECKSUM_SHA256='b661229f0457939d3867cca1664c5671ae959b309feb58abe9142229c8c7ffb4'
+
+# from makefile: -lxcb -lxcb-util -lxcb-icccm -lxcb-ewmh -lxcb-xtest
+deps=('libxcb' 'xcb_util' 'xcb_util_wm')
+makedeps=()
+
+build() {
+    cd "${NAME}-${VERSION}"
+    make PREFIX=/usr
+}
+
+package() {
+    cd "${NAME}-${VERSION}"
+    make PREFIX=/usr DESTDIR="$bindir" install
+    install -Dm644 LICENSE "${bindir}/usr/share/licenses/${NAME}/LICENSE"
+}
