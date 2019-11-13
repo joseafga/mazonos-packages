9bfa5dbb484999ebb94b2f9897cfca6dcaeba030
José Almeida <jose.afga@gmail.com>
2019-11-12 23:03:40 -0300

    improve using npm

--- yarn.mzb.sh
+++ yarn.mzb.sh
@@ -1,29 +1,24 @@
 #!/usr/bin/mzbuild
-# yarn mzbuild 0.2.0a
+# yarn mzbuild 0.3.0a
 #
 # contribuitors:
 #   José Almeida <jose.afga@gmail.com>
 
 NAME='yarn'
 VERSION="${VERSION:-1.19.1}"
-BUILD="${BUILD:-1}"
+BUILD="${BUILD:-2}"
 DESCRIPTION='Fast, reliable, and secure dependency management.'
 LICENSE='BSD'
 HOMEPAGE='https://yarnpkg.com'
 MAINTAINER='José Almeida <jose.afga@gmail.com>'
-URL="https://github.com/yarnpkg/yarn/releases/download/v${VERSION}/yarn-v${VERSION}.tar.gz"
-CHECKSUM_MD5='4d406e34b2461c4c4f2c3d1f93692bf8'
+DOWNLOAD='false' # skip download
 
 deps=('nodejs')
 makedeps=()
 
+# nothing to extract, override with empty funtion
+extract() { :; }
+
 package() {
-    cd "${NAME}-v${VERSION}"
-
-    mkdir -p "${bindir}/usr/lib/node_modules/yarn" "${bindir}/usr/bin"
-    cp -rp * "${bindir}/usr/lib/node_modules/yarn"
-    install -Dm644 -t "${bindir}/usr/share/licenses/${NAME}/" 'LICENSE'
-
-    # create symlinks to execute globally
-    ln -s '/usr/lib/node_modules/yarn/bin/'{yarn,yarnpkg} "${bindir}/usr/bin/"
+    npm -g --prefix "${bindir}/usr" install "yarn@${VERSION}"
 }
